2c211aa258c3264ba8ed586c5b9dafb83b06936a
Jorgsaa <jorgen.saanum@gmail.com>
2022-03-07 14:07:28 +0100

    CHORE: initialized project with IntelliJ

---
+++ src/main/java/com/example/humansvszombiesbackend/HumansVsZombiesBackendApplication.java
@@ -0,0 +1,18 @@
+package com.example.humansvszombiesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+// TODO: Enable security when configured
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+// TODO: Configure CORS
+@CrossOrigin("*")
+public class HumansVsZombiesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumansVsZombiesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/humansvszombiesbackend/HumansVsZombiesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.humansvszombiesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumansVsZombiesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
