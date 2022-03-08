3bb38889044021a5b271c80177157a7d9a2d9ef6
Jorgsaa <jorgen.saanum@gmail.com>
2022-03-08 09:39:51 +0100

    DOCS: added springdoc dependency and enabled web MVC for swagger-ui

--- src/main/java/com/example/humansvszombiesbackend/HumansVsZombiesBackendApplication.java
+++ src/main/java/com/example/humansvszombiesbackend/HumansVsZombiesBackendApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 // TODO: Enable security when configured
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
