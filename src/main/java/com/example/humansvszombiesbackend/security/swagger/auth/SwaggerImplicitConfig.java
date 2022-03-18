2d4da6dd1b933d1ce8b6cc52ccd28358a4eedfdb
Jørgen <jorgen.saanum@no.experis.com>
2022-03-18 13:56:33 +0100

    FEAT: added implicit flow config

---
+++ src/main/java/com/example/humansvszombiesbackend/security/swagger/auth/SwaggerImplicitConfig.java
@@ -0,0 +1,52 @@
+package com.example.humansvszombiesbackend.security.swagger.auth;
+
+import com.example.humansvszombiesbackend.security.KeycloakProperties;
+import com.example.humansvszombiesbackend.security.swagger.ApiInfoProvider;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+@ConditionalOnProperty(name = "security.config.implicit-flow", havingValue = "true")
+public class SwaggerImplicitConfig {
+
+    private static final String OAUTH_SCHEME_NAME = "oAuth";
+    private static final String AUTH_URL_FORMAT = "%s/realms/%s/protocol/openid-connect/auth";
+
+    @Bean
+    OpenAPI customOpenApi(KeycloakProperties keycloakProperties, ApiInfoProvider infoProvider) {
+        return new OpenAPI()
+                .info(infoProvider.provide())
+                .components(new Components()
+                        .addSecuritySchemes(OAUTH_SCHEME_NAME, createOAuthScheme(keycloakProperties)))
+                .addSecurityItem(new SecurityRequirement().addList(OAUTH_SCHEME_NAME));
+    }
+
+    private SecurityScheme createOAuthScheme(KeycloakProperties properties) {
+        OAuthFlows flows = createOAuthFlows(properties);
+
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.OAUTH2)
+                .flows(flows);
+    }
+
+    private OAuthFlows createOAuthFlows(KeycloakProperties properties) {
+        OAuthFlow implicitFlow = createImplicitFlow(properties);
+
+        return new OAuthFlows()
+                .implicit(implicitFlow);
+    }
+
+    private OAuthFlow createImplicitFlow(KeycloakProperties properties) {
+        var authUrl = String.format(AUTH_URL_FORMAT, properties.getAuthServerUrl(), properties.getRealm());
+
+        return new OAuthFlow()
+                .authorizationUrl(authUrl)
+                .scopes(new Scopes());
+    }
+}
