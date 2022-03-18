1812a8e37ccbabca3581ba3ec16bd3270ee6b1b8
Jørgen <jorgen.saanum@no.experis.com>
2022-03-18 13:55:40 +0100

    REFACTOR: renamed `SwaggerAuthConfig` -> `SwaggerBearerConfig`

--- src/main/java/com/example/humansvszombiesbackend/security/swagger/auth/SwaggerBearerConfig.java
+++ src/main/java/com/example/humansvszombiesbackend/security/swagger/auth/SwaggerBearerConfig.java
@@ -1,16 +1,20 @@
-package com.example.humansvszombiesbackend.security;
+package com.example.humansvszombiesbackend.security.swagger.auth;
 
+import com.example.humansvszombiesbackend.security.swagger.ApiInfoProvider;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition
-public class SwaggerAuthConfig {
+@ConditionalOnProperty(name = "security.config.bearer", havingValue = "true")
+public class SwaggerBearerConfig {
+
     private static final String SCHEME_NAME = "bearerAuth";
     private static final String SCHEME = "bearer";
 
