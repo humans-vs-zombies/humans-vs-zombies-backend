1d8d7e0ff183535a0a83724bf960db0a3b2d6f8f
Jorgsaa <jorgen.saanum@gmail.com>
2022-03-21 14:03:27 +0100

    REFACTOR: changed endpoint paths to match specification, now uses custom generated repository methods

--- src/main/java/com/example/humansvszombiesbackend/repository/PlayerRepository.java
+++ src/main/java/com/example/humansvszombiesbackend/repository/PlayerRepository.java
@@ -4,7 +4,14 @@ import com.example.humansvszombiesbackend.model.dbo.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Integer> {
+
+    List<Player> findPlayersByCurrentGameId(Integer id);
+
+    Optional<Player> findPlayersByCurrentGameIdAndId(Integer gameId, Integer id);
 
 }
