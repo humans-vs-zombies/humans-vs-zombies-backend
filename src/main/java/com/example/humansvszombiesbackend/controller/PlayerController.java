e413a520cbed3d33f2edb87dcec74dca7345bc70
Jorgsaa <jorgen.saanum@gmail.com>
2022-03-21 10:34:55 +0100

    FEAT: findAllPlayers now returns non-sensitive information to non-admins

--- src/main/java/com/example/humansvszombiesbackend/controller/PlayerController.java
+++ src/main/java/com/example/humansvszombiesbackend/controller/PlayerController.java
@@ -1,13 +1,17 @@
 package com.example.humansvszombiesbackend.controller;
 
-import com.example.humansvszombiesbackend.model.dbo.Player;
+import com.example.humansvszombiesbackend.model.dto.PlayerDTO;
 import com.example.humansvszombiesbackend.model.dto.Response;
 import com.example.humansvszombiesbackend.repository.PlayerRepository;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -20,9 +24,12 @@ public class PlayerController {
     private final PlayerRepository players;
 
     @GetMapping
-    public ResponseEntity<Response<List<Player>>> findAllPlayers()
+    public ResponseEntity<Response<List<?>>> findAllPlayers(KeycloakAuthenticationToken token)
     {
-        return ResponseEntity.ok(new Response<>(players.findAll()));
+        if (token != null && token.getAuthorities().stream()
+                .anyMatch(authority -> authority.getAuthority().equals("ROLE_admin")))
+            return ResponseEntity.ok(new Response<>(players.findAll()));
+        return ResponseEntity.ok(new Response<>(players.findAll().stream().map(PlayerDTO::from).toList()));
     }
 
     @GetMapping("{id}")
