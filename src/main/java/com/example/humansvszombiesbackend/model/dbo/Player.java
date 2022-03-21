f9efee5475ea319cc8fd9af4a90bcfbb0dc8ebbd
Jorgsaa <jorgen.saanum@gmail.com>
2022-03-21 13:04:54 +0100

    FEAT: added `@JsonIgnore` and `@JsonGetter` for Player::currentGame. TODO added for mapping to DTO

--- src/main/java/com/example/humansvszombiesbackend/model/dbo/Player.java
+++ src/main/java/com/example/humansvszombiesbackend/model/dbo/Player.java
@@ -1,5 +1,7 @@
 package com.example.humansvszombiesbackend.model.dbo;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
@@ -18,16 +20,24 @@ public class Player {
     private Integer id;
 
     @Column(nullable = false)
+    @Builder.Default
     private boolean isHuman = true;
 
     @Column
     private String biteCode;
 
+    @JsonIgnore
     @ManyToOne
     @JoinColumn
     private Game currentGame;
 
     @Column(nullable = false)
     private UUID userId;
+
+    //TODO: map to DTO
+    @JsonGetter
+    public Game currentGame() {
+        return currentGame;
+    }
 
 }
