70cc2e4a2485bfc0e336e8e5b55107b5b50efb62
Jørgen <jorgen.saanum@no.experis.com>
2022-03-24 00:45:16 +0100

    FEAT: added SquadCreateDTO for sending Squad create requests

---
+++ src/main/java/com/example/humansvszombiesbackend/model/dto/SquadCreateDTO.java
@@ -0,0 +1,12 @@
+package com.example.humansvszombiesbackend.model.dto;
+
+import lombok.Data;
+
+@Data
+public class SquadCreateDTO {
+
+    private String name;
+
+    private Integer playerId;
+
+}
