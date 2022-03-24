636d7f5dd5685b830c83f0516eaadc50f6604536
Jørgen <jorgen.saanum@no.experis.com>
2022-03-24 21:10:56 +0100

    FEAT: added auto-generated method to SquadCheckInRepository

--- src/main/java/com/example/humansvszombiesbackend/repository/SquadCheckInRepository.java
+++ src/main/java/com/example/humansvszombiesbackend/repository/SquadCheckInRepository.java
@@ -3,5 +3,10 @@ package com.example.humansvszombiesbackend.repository;
 import com.example.humansvszombiesbackend.model.dbo.SquadCheckIn;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SquadCheckInRepository extends JpaRepository<SquadCheckIn, Integer> {
+
+    List<SquadCheckIn> findAllByGameIdAndSquadId(Integer gameId, Integer squadId);
+
 }
